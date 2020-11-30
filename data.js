1ec3f78048d84f7b4c52599373d8024be6f6024f
mabadinakach <mabadinakach@gmail.com>
2020-11-30 17:09:00 -0600

    added google calendar functionality

--- data.js
+++ data.js
@@ -1,59 +1,108 @@
-const data = [
-    {
-         class: 'CS 1.1',
-         assignments: [
-             'The Flower Garden - 21.0 / 24.0',
-             'Quiz 1 - 21.0 / 21.0',
-             'Superhero Team Dueler - 8.0 / 10.0',
-             'Quiz 2 - 25.0 / 25.0'
-         ],
-         grade: 91,
-         missing: [
-             "Superhero Dueler",
-             "Superhero Dueler",
-             "Superhero Dueler",
-         ],
-         link: "/courses/165363"
-    },
-    {
-        class: 'CS 1.2',
-        assignments : [
-            'Quiz 1 - 14.0 / 14.0'
-        ],
-        grade: 100,
-        link: "/courses/202248"
-    },
-    {
-        class: 'QL1.1',
-        assignments: [
-            'Mean, Median and Mode - 5.0 / 6.0',
-            'Median and Mode Algorithms - 20.0 / 20.0',
-            'Sample, Population, z-score - 8.0 / 8.0'
-        ],
-        grade: 90,
-        link: "/courses/202986"
-    },
-    {
-        class:'SPD 1.2',
-        assignments: [
-            'Bootstrap Product Landing Page - 1.0 / 1.0',
-            'Clone a Webpage - 1.0 / 1.0',
-            'Capstone Bootstrap Site - 1.0 / 1.0',
-            'Digital Mockup - 1.0 / 1.0'
-        ],
-        grade: 97,
-        missing: [
-            "Sprint 2",
-        ],
-        link: "/courses/200791"
-    },
-    // {
-    //     class: 'Archived - WEB 1.0 (Term 1)',
-    //     assignments: {
-    //         'Portfolio Part 1 (HTML)': '9.0 / 9.0', 'Learn Semantic Markup': '2.5 / 3.0', 'Learn Markup Attributes (Level 2)': '0.9 / 1.0', 'Assignment 4.1 - CSS Challenges 1': '3.0 / 3.0', 'Assignment 4.2 - CSS Challenges 2': '2.0 / 2.0', 'Assignment 4.3 - CSS Portfolio': '6.0 / 9.0', 'Single Page Site': '6.0 / 9.0', 'Tip Calculator': '4.0 / 4.0', 'Mood Shop Tutorial Parts 1-12': '3.0 / 3.0', 'Custom Calculator': '4.0 / 4.0', 'Final Assessment': '3.0 / 3.0'
-    //     },
-    //     grade: 100,
-    //     link: "/courses/166306"
-    // }
-]
+// const data = [
+//     {
+//          class: 'CS 1.1',
+//          assignments: [
+//              'The Flower Garden - 21.0 / 24.0',
+//              'Quiz 1 - 21.0 / 21.0',
+//              'Superhero Team Dueler - 8.0 / 10.0',
+//              'Quiz 2 - 25.0 / 25.0'
+//          ],
+//          grade: 91,
+//          missing: [
+//              "Superhero Dueler",
+//              "Superhero Dueler",
+//              "Superhero Dueler",
+//          ],
+//          link: "/courses/165363"
+//     },
+//     {
+//         class: 'CS 1.2',
+//         assignments : [
+//             'Quiz 1 - 14.0 / 14.0'
+//         ],
+//         grade: 100,
+//         link: "/courses/202248"
+//     },
+//     {
+//         class: 'QL1.1',
+//         assignments: [
+//             'Mean, Median and Mode - 5.0 / 6.0',
+//             'Median and Mode Algorithms - 20.0 / 20.0',
+//             'Sample, Population, z-score - 8.0 / 8.0'
+//         ],
+//         grade: 90,
+//         link: "/courses/202986"
+//     },
+//     {
+//         class:'SPD 1.2',
+//         assignments: [
+//             'Bootstrap Product Landing Page - 1.0 / 1.0',
+//             'Clone a Webpage - 1.0 / 1.0',
+//             'Capstone Bootstrap Site - 1.0 / 1.0',
+//             'Digital Mockup - 1.0 / 1.0'
+//         ],
+//         grade: 97,
+//         missing: [
+//             "Sprint 2",
+//         ],
+//         link: "/courses/200791"
+//     },
+//     // {
+//     //     class: 'Archived - WEB 1.0 (Term 1)',
+//     //     assignments: {
+//     //         'Portfolio Part 1 (HTML)': '9.0 / 9.0', 'Learn Semantic Markup': '2.5 / 3.0', 'Learn Markup Attributes (Level 2)': '0.9 / 1.0', 'Assignment 4.1 - CSS Challenges 1': '3.0 / 3.0', 'Assignment 4.2 - CSS Challenges 2': '2.0 / 2.0', 'Assignment 4.3 - CSS Portfolio': '6.0 / 9.0', 'Single Page Site': '6.0 / 9.0', 'Tip Calculator': '4.0 / 4.0', 'Mood Shop Tutorial Parts 1-12': '3.0 / 3.0', 'Custom Calculator': '4.0 / 4.0', 'Final Assessment': '3.0 / 3.0'
+//     //     },
+//     //     grade: 100,
+//     //     link: "/courses/166306"
+//     // }
+// ]
+
+//const data = [{'class': 'CS 1.1', 'assignments': ['The Flower Garden - 21.0 / 24.0', 'Quiz 1 - 21.0 / 21.0', 'Superhero Team Dueler - 8.0 / 10.0', 'Quiz 2 - 25.0 / 25.0'], 'missing': [], 'grade': 91, 'link': 'https://www.gradescope.com/courses/165363'}, {'class': 'CS 1.2', 'assignments': ['Quiz 1 - 14.0 / 14.0'], 'missing': [], 'grade': 100, 'link': 'https://www.gradescope.com/courses/202248'}, {'class': 'INT 1.1', 'assignments': [], 'missing': [], 'grade': 0, 'link': 'https://www.gradescope.com/courses/195384'}, {'class': 'QL1.1', 'assignments': ['Mean, Median and Mode - 5.0 / 6.0', 'Median and Mode Algorithms - 20.0 / 20.0', 'Sample, Population, z-score - 8.0 / 8.0'], 'missing': [], 'grade': 94, 'link': 'https://www.gradescope.com/courses/202986'}, {'class': 'SPD 1.2', 'assignments': ['Bootstrap Product Landing Page - 1.0 / 1.0', 'Clone a Webpage - 1.0 / 1.0', 'Capstone Bootstrap Site - 1.0 / 1.0', 'Digital Mockup - 1.0 / 1.0'], 'missing': ['Project Sprint 2'], 'grade': 100, 'link': 'https://www.gradescope.com/courses/200791'}, {'class': 'Archived - WEB 1.0 (Term 1)', 'assignments': ['Portfolio Part 1 (HTML) - 9.0 / 9.0', 'Learn Semantic Markup - 2.5 / 3.0', 'Learn Markup Attributes (Level 2) - 0.9 / 1.0', 'Assignment 4.1 - CSS Challenges 1 - 3.0 / 3.0', 'Assignment 4.2 - CSS Challenges 2 - 2.0 / 2.0', 'Assignment 4.3 - CSS Portfolio - 6.0 / 9.0', 'Single Page Site - 6.0 / 9.0', 'Tip Calculator - 4.0 / 4.0', 'Mood Shop Tutorial Parts 1-12 - 3.0 / 3.0', 'Custom Calculator - 4.0 / 4.0', 'Final Assessment - 3.0 / 3.0'], 'missing': [], 'grade': 91, 'link': 'https://www.gradescope.com/courses/166306'}]
+const data = [{
+	'class': 'CS 1.1',
+	'assignments': ['The Flower Garden - 21.0 / 24.0', 'Quiz 1 - 21.0 / 21.0', 'Superhero Team Dueler - 8.0 / 10.0', 'Quiz 2 - 25.0 / 25.0'],
+	'missing': [],
+	'grade': 91,
+	'test': [],
+	'link': 'https://www.gradescope.com/courses/165363'
+}, {
+	'class': 'CS 1.2',
+	'assignments': ['Quiz 1 - 14.0 / 14.0'],
+	'missing': [],
+	'grade': 100,
+	'test': [],
+	'link': 'https://www.gradescope.com/courses/202248'
+}, {
+	'class': 'INT 1.1',
+	'assignments': [],
+	'missing': [],
+	'grade': 0,
+	'test': [],
+	'link': 'https://www.gradescope.com/courses/195384'
+}, {
+	'class': 'QL1.1',
+	'assignments': ['Mean, Median and Mode - 5.0 / 6.0', 'Median and Mode Algorithms - 20.0 / 20.0', 'Sample, Population, z-score - 8.0 / 8.0'],
+	'missing': [],
+	'grade': 94,
+	'test': [],
+	'link': 'https://www.gradescope.com/courses/202986'
+}, {
+	'class': 'SPD 1.2',
+	'assignments': ['Bootstrap Product Landing Page - 1.0 / 1.0', 'Clone a Webpage - 1.0 / 1.0', 'Capstone Bootstrap Site - 1.0 / 1.0', 'Digital Mockup - 1.0 / 1.0'],
+	'missing': ['Project Sprint 2', 'Project Sprint 3'],
+	'grade': 100,
+	'test': [{
+		'Project Sprint 2': 'Nov 30 2020'
+	}, {
+		'Project Sprint 3': 'Dec 07 2020'
+	}],
+	'link': 'https://www.gradescope.com/courses/200791'
+}, {
+	'class': 'Archived - WEB 1.0 (Term 1)',
+	'assignments': ['Portfolio Part 1 (HTML) - 9.0 / 9.0', 'Learn Semantic Markup - 2.5 / 3.0', 'Learn Markup Attributes (Level 2) - 0.9 / 1.0', 'Assignment 4.1 - CSS Challenges 1 - 3.0 / 3.0', 'Assignment 4.2 - CSS Challenges 2 - 2.0 / 2.0', 'Assignment 4.3 - CSS Portfolio - 6.0 / 9.0', 'Single Page Site - 6.0 / 9.0', 'Tip Calculator - 4.0 / 4.0', 'Mood Shop Tutorial Parts 1-12 - 3.0 / 3.0', 'Custom Calculator - 4.0 / 4.0', 'Final Assessment - 3.0 / 3.0'],
+	'missing': [],
+	'grade': 91,
+	'test': [],
+	'link': 'https://www.gradescope.com/courses/166306'
+}]
 export default data;
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-import data from './data.js'
 
+import data from './data.js'
 const itemsContainer = document.getElementById('items')
+
+window.onload = handleClientLoad()
 
 let container = document.createElement('div')
 container.className = "container"
@@ -13,7 +15,7 @@ for (let i = 0; i<data.length; i++) {
     //     brake.className = "w-100"
     //     row.appendChild(brake)
     // }
-
+    
     let column = document.createElement('div')
     column.className = "col"
     let card = document.createElement('div')
@@ -37,7 +39,7 @@ for (let i = 0; i<data.length; i++) {
     missingAssignments.setAttribute("aria-expanded", "false");
     missingAssignments.style.margin = "10px"
     missingAssignments.style.backgroundColor = "red"
-
+    
     let gradedAssignments = document.createElement('a')
     gradedAssignments.innerHTML = "Graded Assignments"
     gradedAssignments.className = "btn btn-primary"
@@ -48,16 +50,17 @@ for (let i = 0; i<data.length; i++) {
     gradedAssignments.setAttribute("aria-expanded", "false");
     gradedAssignments.style.backgroundColor = "green"
     
-
+    
     
     dropDown.appendChild(gradedAssignments)
-
-
+    
+    
     let infoRow = document.createElement('div')
     infoRow.className = "row"
     let gradedCol = document.createElement('div')
     gradedCol.className = "col"
     let colapseDiv = document.createElement('div')
+    colapseDiv.style.margin = "10px"
     colapseDiv.className = "collapse multi-collapse"
     colapseDiv.id = "gradedAssignments"+i
     
@@ -66,42 +69,48 @@ for (let i = 0; i<data.length; i++) {
         gradedDiv.innerHTML = data[i]["assignments"][k]
         colapseDiv.appendChild(gradedDiv)
     }
-
+    
     // gradedDiv.id = "missingAssignments"
     colapseDiv.className = "card card-body"
     gradedCol.appendChild(colapseDiv)
     infoRow.append(gradedCol)
-        
-
+    
+    
     title.className = "card-title"
     title.innerHTML = data[i]["class"]
-
+    
     grade.className = "card-text"
     //grade.style.color = "green"
     grade.innerHTML = `Grade: ${data[i]["grade"]}`
-
+    
     button.className = "btn btn-primary"
     button.innerHTML = "Details"
     button.id = i
     
-
+    
     cardBody.appendChild(title)
     cardBody.appendChild(grade)
-
     
-
+    Object.entries(data[i]["test"]).forEach(([key, value]) => {
+        console.log(Object.keys(data[i]["test"][key])[0])
+        console.log(Object.values(value)[0])
+        console.log(Date(Object.values(value)[0]))
+    });
+    
+    
     if(data[i]["missing"] != null){
         
         let missingCol = document.createElement('div')
         missingCol.className = "col"
         let colapseDiv = document.createElement('div')
+        colapseDiv.style.margin = "10px"
         colapseDiv.className = "collapse multi-collapse"
         colapseDiv.id = "missingAssignments"+i
         
         // gradedDiv.id = "missingAssignments"
         colapseDiv.className = "card card-body"
         cardBody.appendChild(infoRow)
-
+        
         // let text = document.createElement('h5')
         // text.innerHTML = "Missing Assignments:"
         // cardBody.appendChild(text)
@@ -117,13 +126,13 @@ for (let i = 0; i<data.length; i++) {
             
             colapseDiv.appendChild(missingDiv)
         }
-
+        
         missingCol.appendChild(colapseDiv)
         infoRow.append(missingCol)
         dropDown.appendChild(missingAssignments)
-
+        
     }
-
+    
     
     cardBody.appendChild(dropDown)
     cardBody.appendChild(infoRow)
@@ -131,7 +140,7 @@ for (let i = 0; i<data.length; i++) {
     center.className = "text-center"
     center.appendChild(button)
     cardBody.appendChild(center)
-
+    
     card.appendChild(cardBody)
     card.style.margin = "10px"
     card.style.minHeight = "150px"
@@ -139,7 +148,7 @@ for (let i = 0; i<data.length; i++) {
     column.appendChild(card)
     row.appendChild(column)
     container.appendChild(row)
-
+    
     //itemsContainer.appendChild(card)
 }
 itemsContainer.appendChild(container)
@@ -151,5 +160,175 @@ console.log(all_items_button)
 all_items_button.forEach(elt => elt.addEventListener('click', (index) => {
     console.log(elt.getAttribute('id'))
     console.log(data[elt.getAttribute('id')])
-    window.open("https://www.gradescope.com"+data[elt.getAttribute('id')]["link"])
+    window.open(data[elt.getAttribute('id')]["link"])
 }))
+
+
+var CLIENT_ID = '486906308520-7gapsff55hia8hj8kvgk3mdctjojtilo.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyChNbkCvbk2KBayw2nzgaz-bW5BveZOsO4';
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = "https://www.googleapis.com/auth/calendar";
+
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
+
+/**
+*  On load, called to load the auth2 library and API client library.
+*/
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+}
+
+/**
+*  Initializes the API client library and sets up sign-in state
+*  listeners.
+*/
+function initClient() {
+    gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+    }).then(function () {
+        // Listen for sign-in state changes.
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+        
+        // Handle the initial sign-in state.
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick = handleSignoutClick;
+    }, function(error) {
+        appendPre(JSON.stringify(error, null, 2));
+    });
+}
+
+/**
+*  Called when the signed in status changes, to update the UI
+*  appropriately. After a sign-in, the API is called.
+*/
+function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+        authorizeButton.style.display = 'none';
+        signoutButton.style.display = 'block';
+        listUpcomingEvents();
+    } else {
+        authorizeButton.style.display = 'block';
+        signoutButton.style.display = 'none';
+    }
+}
+
+/**
+*  Sign in the user upon button click.
+*/
+function handleAuthClick(event) {
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+*  Sign out the user upon button click.
+*/
+function handleSignoutClick(event) {
+    gapi.auth2.getAuthInstance().signOut();
+}
+
+/**
+* Append a pre element to the body containing the given message
+* as its text node. Used to display the results of the API call.
+*
+* @param {string} message Text to be placed in pre element.
+*/
+function appendPre(message) {
+    var pre = document.getElementById('content');
+    var textContent = document.createTextNode(message + '\n');
+    pre.appendChild(textContent);
+}
+
+/**
+* Print the summary and start datetime/date of the next ten events in
+* the authorized user's calendar. If no events are found an
+* appropriate message is printed.
+*/
+function listUpcomingEvents() {
+    
+    for (let i = 0; i<data.length; i++) {
+        Object.entries(data[i]["test"]).forEach(([key, value]) => {
+            console.log(Object.keys(data[i]["test"][key])[0])
+            console.log(Object.values(value)[0])
+            var date = Date.parse(Object.values(value)[0])
+            var dateEpoch = new Date(date)
+            var year = dateEpoch.getFullYear();
+            var month = dateEpoch.getMonth() + 1;
+            var day = dateEpoch.getDate();
+            var dateTime = year + "-" + month + "-" + day
+            console.log(dateTime)
+            var event = {
+                'summary': `${data[i]["class"]} - ${Object.keys(data[i]["test"][key])[0]}`,
+                'location': '',
+                'description': `${Object.keys(data[i]["test"][key])[0]} due today`,
+                'start': {
+                    'dateTime': dateTime+'T22:00:00-07:00',
+                    'timeZone': 'America/Los_Angeles'
+                },
+                'end': {
+                    'dateTime': dateTime+'T22:59:00-07:00',
+                    'timeZone': 'America/Los_Angeles'
+                },
+                'recurrence': [
+                    // 'RRULE:FREQ=DAILY;COUNT=2'
+                ],
+                'attendees': [
+                    
+                ],
+                'reminders': {
+                    'useDefault': false,
+                    'overrides': [
+                        {'method': 'popup', 'minutes': 1440},
+                        {'method': 'popup', 'minutes': 10},
+                    ]
+                }
+            };
+
+            var request = gapi.client.calendar.events.insert({
+                'calendarId': 'primary',
+                'resource': event
+            });
+            
+            gapi.client.calendar.events.list({
+                'calendarId': 'primary',
+                'timeMin': (new Date()).toISOString(),
+                'showDeleted': false,
+                'singleEvents': true,
+                'maxResults': 30,
+                'orderBy': 'startTime'
+            }).then(function(response) {
+                var events = response.result.items;
+            
+                if (events.length > 0) {
+                    for (let j = 0; j < events.length; j++) {
+                        var event = events[j];
+                        var when = event.start.dateTime;
+                        if (!when) {
+                            when = event.start.date;
+                        }
+                        if (event.summary == `${data[i]["class"]} - ${Object.keys(data[i]["test"][key])[0]}`) {
+                            console.log("existe")
+                            return
+                        } 
+                        //appendPre(event.summary + ' (' + when + ')')
+                    }
+                    request.execute(function(event) {
+                        appendPre('Event added to calendar: ' + `${data[i]["class"]} - ${Object.keys(data[i]["test"][key])[0]}`);
+                    });
+                } else {
+                    //appendPre('No upcoming events found.');
+                }
+            });
+                
+        });
+    }   
+}
